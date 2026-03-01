e65e553d4bc5edbbbadd0d0cd8bcef25b6b60667
Diego <diegoneves2021123@gmail.com>
2026-03-01 16:59:44 -0300

    atualização

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => ({
   base: mode === 'github'
     ? '/Nxfox/'
     : '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        clientes: resolve(__dirname, 'clientes.html'),
+        licencas: resolve(__dirname, 'licencas.html'),
+      }
+    }
+  },
   server: {
     port: 5173,
     open: true
